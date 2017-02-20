445a962288008a047ac7c9aecb335eb06733bdef
Onur Sumer <s.onur.sumer@gmail.com>
2017-02-20 10:42:16 -0500

    added spec files for OncoKB component (no tests implemented yet)

---
+++ src/shared/components/annotation/OncoKB.spec.jsx
@@ -0,0 +1,21 @@
+import * as OncoKB from './OncoKB';
+import React from 'react';
+import { assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+
+describe('OncoKB', () => {
+
+    before(()=>{
+
+    });
+
+    after(()=>{
+
+    });
+
+    it('what does it do?', ()=>{
+
+    });
+
+});
