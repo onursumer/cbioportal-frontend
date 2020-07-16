8a8fa1a8509fbac8471bfa0716d59927fa4bb1d5
Merve <kilicarslanmerve@gmail.com>
2020-07-17 02:16:57 +0300

    add PathwayMapper to Patient View

---
+++ src/pages/patientView/pathwayMapper/PatientViewPathwayMapper.tsx
@@ -0,0 +1,159 @@
+import * as React from 'react';
+import { PatientViewPageStore } from '../clinicalInformation/PatientViewPageStore';
+import PathwayMapperTable, {
+    IPathwayMapperTable,
+    IPathwayMapperTableColumnType,
+} from '../../../shared/lib/pathwayMapper/PathwayMapperTable';
+import {
+    getCnaTypes,
+    getUniqueGenes,
+} from 'shared/lib/pathwayMapper/PathwayMapperHelpers';
+import { observer } from 'mobx-react';
+import autobind from 'autobind-decorator';
+import { observable, computed } from 'mobx';
+import { Row } from 'react-bootstrap';
+
+import { getGeneticTrackRuleSetParams } from 'shared/components/oncoprint/OncoprintUtils';
+import PathwayMapper, { ICBioData } from 'pathway-mapper';
+
+import 'pathway-mapper/dist/base.css';
+import 'cytoscape-panzoom/cytoscape.js-panzoom.css';
+import 'cytoscape-navigator/cytoscape.js-navigator.css';
+
+interface IPatientViewPathwayMapperProps {
+    store: PatientViewPageStore;
+}
+
+const DEFAULT_RULESET_PARAMS = getGeneticTrackRuleSetParams(true, true, true);
+
+@observer
+export default class PatientViewPathwayMapper extends React.Component<
+    IPatientViewPathwayMapperProps
+> {
+    constructor(props: IPatientViewPathwayMapperProps) {
+        super(props);
+
+        // @ts-ignore
+        import(/* webpackChunkName: "pathway-mapper" */ 'pathway-mapper').then(
+            (module: any) => {
+                this.PathwayMapperComponent = (module as any)
+                    .default as PathwayMapper;
+            }
+        );
+    }
+
+    @observable.ref PathwayMapperComponent:
+        | PathwayMapper
+        | undefined = undefined;
+
+    @computed get queryGenes() {
+        return getUniqueGenes(this.alterationFrequencyData);
+    }
+
+    @computed get alterationFrequencyData(): ICBioData[] {
+        return this.alterationFrequencyDataForQueryGenes;
+    }
+
+    @computed get alterationFrequencyDataForQueryGenes() {
+        const alterationFrequencyData: ICBioData[] = [];
+
+        this.props.store.mergedMutationDataIncludingUncalledFilteredByGene.forEach(
+            altData => {
+                const mutationType = {
+                    gene: altData[0].gene.hugoGeneSymbol,
+                    altered: 1,
+                    sequenced: 1,
+                    percentAltered: altData[0].mutationType,
+                    geneticTrackRuleSetParams: DEFAULT_RULESET_PARAMS,
+                    geneticTrackData: this.props.store.geneticTrackData.result
+                        ? this.props.store.geneticTrackData.result[
+                              altData[0].gene.hugoGeneSymbol
+                          ]
+                        : undefined,
+                };
+                if (mutationType) {
+                    alterationFrequencyData.push(mutationType);
+                }
+            }
+        );
+
+        this.props.store.mergedDiscreteCNADataFilteredByGene.forEach(
+            altData => {
+                const cna = {
+                    gene: altData[0].gene.hugoGeneSymbol,
+                    altered: 1,
+                    sequenced: 1,
+                    percentAltered: getCnaTypes(altData[0].alteration),
+                    geneticTrackRuleSetParams: DEFAULT_RULESET_PARAMS,
+                    geneticTrackData: this.props.store.geneticTrackData.result
+                        ? this.props.store.geneticTrackData.result[
+                              altData[0].gene.hugoGeneSymbol
+                          ]
+                        : undefined,
+                };
+                if (cna) {
+                    alterationFrequencyData.push(cna);
+                }
+            }
+        );
+
+        return alterationFrequencyData;
+    }
+
+    public render() {
+        if (!this.PathwayMapperComponent) {
+            return null;
+        }
+        return (
+            <div className="pathwayMapper">
+                <div
+                    data-test="pathwayMapperTabDiv"
+                    className="cBioMode"
+                    style={{ width: '99%' }}
+                >
+                    <Row>
+                        {/*
+                              // @ts-ignore */}
+                        <this.PathwayMapperComponent
+                            isCBioPortal={true}
+                            isCollaborative={false}
+                            genes={this.queryGenes}
+                            cBioAlterationData={this.alterationFrequencyData}
+                            tableComponent={this.renderTable}
+                            patientView={true}
+                            //message banner patch will be removed
+                            messageBanner={this.renderBanner}
+                        />
+                    </Row>
+                </div>
+            </div>
+        );
+    }
+
+    //will be removed when Banner is added
+    @autobind
+    private renderBanner() {
+        return null;
+    }
+
+    @autobind
+    private renderTable(
+        data: IPathwayMapperTable[],
+        selectedPathway: string,
+        onPathwaySelect: (pathway: string) => void
+    ) {
+        return (
+            <PathwayMapperTable
+                data={data}
+                selectedPathway={selectedPathway}
+                changePathway={onPathwaySelect}
+                columnsOverride={{
+                    [IPathwayMapperTableColumnType.SCORE]: {
+                        name: '# Genes matched',
+                        tooltip: <span>Number of Genes Matched</span>,
+                    },
+                }}
+            />
+        );
+    }
+}
--- src/shared/lib/pathwayMapper/PathwayMapperHelpers.ts
+++ src/shared/lib/pathwayMapper/PathwayMapperHelpers.ts
@@ -1,3 +1,6 @@
+import { ICBioData } from 'pathway-mapper';
+import _ from 'lodash';
+
 // Calculates truncated version of gene string without truncating gene names.
 // That is, instead of truncating a gene name, removes it all.
 // For example, if the gene string is MDM2 CDKN2A, instead of truncating it as "MDM2 CKDN ...",
@@ -20,4 +23,25 @@ export function truncateGeneList(
     }
 
     return geneStr.trim();
+}
+
+export function getUniqueGenes(data: ICBioData[]) {
+    return _.uniq(data.map(x => x.gene)).map(gene => ({
+        hugoGeneSymbol: gene,
+    }));
+}
+
+export function getCnaTypes(cnaType: number) {
+    switch (cnaType) {
+        case -2:
+            return 'DeepDel';
+        case -1:
+            return 'SHALLOWDEL';
+        case 0:
+            return 'DIPLOID';
+        case 1:
+            return 'GAIN';
+        default:
+            return 'AMP';
+    }
 }
