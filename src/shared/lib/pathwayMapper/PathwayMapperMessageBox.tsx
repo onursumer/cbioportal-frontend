14081bb9815f4a4d600986469c2757bb0f68107a
Salih Altun <muhammedsalihaltun@gmail.com>
2020-12-10 16:33:10 +0300

    Replace toast with banner (fix cBioPortal/cbioportal#7677)

---
+++ src/shared/lib/pathwayMapper/PathwayMapperMessageBox.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+interface PathwayMapperMessageBoxProps {
+    message: string;
+    loadingMessage: string;
+    onClearMessage: () => void;
+}
+
+const PathwayMapperMessageBox: React.FunctionComponent<PathwayMapperMessageBoxProps> = props => {
+    const message = props.message;
+    const isWarningMessage = message !== props.loadingMessage;
+    return (
+        <div
+            className={
+                'alert ' +
+                (isWarningMessage ? 'alert-warning' : 'alert-success')
+            }
+            style={{
+                marginLeft: '1%',
+                marginBottom: '0px',
+                color: 'black',
+                maxHeight: '35px',
+                overflowY: 'auto',
+                display: message.length == 0 ? 'none' : 'block',
+            }}
+        >
+            <button
+                type="button"
+                className="close"
+                onClick={props.onClearMessage}
+            >
+                <FontAwesome name="times" />
+            </button>
+            {isWarningMessage && (
+                <i
+                    className="fa fa-md fa-exclamation-triangle"
+                    style={{
+                        marginRight: '6px',
+                        marginBottom: '1px',
+                    }}
+                />
+            )}
+            {message}
+        </div>
+    );
+};
+
+export default PathwayMapperMessageBox;
